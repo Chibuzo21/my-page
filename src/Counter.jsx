7ca380ee9cd55164ec65be879a03fe255d4b67f9
Chibuzo <uchibuzo1@gmail.com>
2024-05-14 12:50:38 +0100

    added stuffs on scrollheader.jsx

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+function Counter() {
+    const[counter,setCounter]= useState(0);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCounter(counter+1);
+    },1000);
+  });
+  return (
+    <>
+     <main className="flex h-screen justify-center items-center ">
+      <h1 className="text-5xl">{counter}</h1>
+     </main>
+
+    </>
+  )
+}
+
+export default Counter
